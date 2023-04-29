d4a346e20d6eb41079b5912763b5c6f9ae55e152
mayankshah97 <mayankshah97@gmail.com>
2023-04-28 23:03:41 -0400

    Update relational_fetch.py

--- relational_fetch.py
+++ relational_fetch.py
@@ -1,96 +1,126 @@
-import csv
-import sqlite3
-import pandas as pd
-import json
-import mysql.connector
 import pymysql
 from ordered_hash_set import OrderedSet
 
-def connect():
-    # establish connection to database
-    conn = pymysql.connect(user='root', \
-        password='Mayank@45', \
-        host='localhost', \
-        database='mydatabase')  
-    cursor = conn.cursor()
+class Relational():
+    def __init__(self) -> None:
+        pass
 
-    return cursor
+    # Method to establish connection the database
+    def connect(self):
+        # establish connection to database
+        conn = pymysql.connect(user='***', \
+            password='***', \
+            host='***', \
+            database='***')  
+        cursor = conn.cursor()
 
-def disconnect(cursor):
-    # cursor = connect()
-    cursor.close()
+        return cursor
 
-def get_users(str_input):
-    cursor = connect()
-    s = OrderedSet()
+    # Method to close the connection the database
+    def disconnect(self,cursor):
+        cursor.close()
 
-    #Query which searches the user account name exactly
-    cursor.execute("SELECT * FROM user WHERE name = '%s' \
-    ORDER BY verified DESC, followers_count DESC"%str_input)
+    # Get all the possible users whenever an user is searched and order them by verified status and number of followers
+    def get_users(self,str_input):
+        cursor = self.connect()
+        s = OrderedSet()
 
-    res = cursor.fetchall()
-    for line in res:
-        s.add(line)
+        #Query which searches the user account name exactly
+        cursor.execute("SELECT * FROM user WHERE name = '%s' \
+        ORDER BY verified DESC, followers_count DESC"%str_input)
 
-    #Query which searches the username exactly
-    cursor.execute("SELECT * FROM user WHERE screen_name = '%s' \
-    ORDER BY verified DESC, followers_count DESC"%str_input)
+        res = cursor.fetchall()
+        for line in res:
+            s.add(line)
 
-    res = cursor.fetchall()
-    for line in res:
-        s.add(line)
+        #Query which searches the username exactly
+        cursor.execute("SELECT * FROM user WHERE screen_name = '%s' \
+        ORDER BY verified DESC, followers_count DESC"%str_input)
 
-    #Query which searches the user account name partially (starts with)
-    qu = "SELECT * FROM user WHERE name LIKE %s\
-    ORDER BY verified DESC, followers_count DESC"
-    cursor.execute(qu, (str_input + "%",))
+        res = cursor.fetchall()
+        for line in res:
+            s.add(line)
+
+        #Query which searches the user account name partially (starts with)
+        qu = "SELECT * FROM user WHERE name LIKE %s\
+        ORDER BY verified DESC, followers_count DESC"
+        cursor.execute(qu, (str_input + "%",))
+        
+        res = cursor.fetchall()
+        for line in res:
+            s.add(line)
+
+        #Query which searches the user username partially (starts with)
+        qu = "SELECT * FROM user WHERE name LIKE %s\
+        ORDER BY verified DESC, followers_count DESC"
+        cursor.execute(qu, (str_input + "%",))
+        
+        res = cursor.fetchall()
+        for line in res:
+            s.add(line)
+
+        #Query which searches the user account name partially
+        qu = "SELECT * FROM user WHERE name LIKE %s\
+        ORDER BY verified DESC, followers_count DESC"
+        cursor.execute(qu, ("%" + str_input + "%",))
+        
+        res = cursor.fetchall()
+        for line in res:
+            s.add(line)
+
+
+        #Query which searches the username partially
+        lo = str_input.lower()
+        qu = ("SELECT * FROM user WHERE screen_name LIKE %s \
+        ORDER BY verified DESC, followers_count DESC")
+        cursor.execute(qu, ("%" + lo + "%",))
+
+        res = cursor.fetchall()
+        for line in res:
+            s.add(line)
+
+        self.disconnect(cursor)
+
+        return list(s)
+
+    # Get the all the details of a particular user
+    def get_user_details(self,str_input):
+        cursor = self.connect()
+
+        #Query which searches the username exactly
+        cursor.execute("SELECT * FROM user WHERE id = '%s' \
+        ORDER BY verified DESC, followers_count DESC"%str_input)
+
+        res = cursor.fetchall()
+        self.disconnect(cursor)
+        return res
     
-    res = cursor.fetchall()
-    for line in res:
-        s.add(line)
+    # Get the top 20 handles, i.e, users having the most number of followers
+    def top_handles(self):
+        cursor = self.connect()
 
-    #Query which searches the user username partially (starts with)
-    qu = "SELECT * FROM user WHERE name LIKE %s\
-    ORDER BY verified DESC, followers_count DESC"
-    cursor.execute(qu, (str_input + "%",))
+        cursor.execute("SELECT * FROM user ORDER BY followers_count DESC LIMIT 20")
+
+        res = cursor.fetchall()
+        self.disconnect(cursor)
+        return list(res)
     
-    res = cursor.fetchall()
-    for line in res:
-        s.add(line)
+    # Get the top 20 celebrities, i.e., users who are verified and having the most number of followers
+    def top_celebs(self):
+        cursor = self.connect()
 
-    #Query which searches the user account name partially
-    qu = "SELECT * FROM user WHERE name LIKE %s\
-    ORDER BY verified DESC, followers_count DESC"
-    cursor.execute(qu, ("%" + str_input + "%",))
-    
-    res = cursor.fetchall()
-    for line in res:
-        s.add(line)
+        cursor.execute("SELECT * FROM user ORDER BY verified DESC, followers_count DESC LIMIT 20")
 
+        res = cursor.fetchall()
+        self.disconnect(cursor)
+        return list(res)
 
-    #Query which searches the username partially
-    lo = str_input.lower()
-    qu = ("SELECT * FROM user WHERE screen_name LIKE %s \
-    ORDER BY verified DESC, followers_count DESC")
-    cursor.execute(qu, ("%" + lo + "%",))
+    # Get the top 20 active users with the most number tweets
+    def top_actives(self):
+        cursor = self.connect()
 
-    res = cursor.fetchall()
-    for line in res:
-        s.add(line)
+        cursor.execute("SELECT * FROM user ORDER BY statuses_count DESC LIMIT 20")
 
-    for i in s:
-        print(i)
-    disconnect(cursor)
-
-    return s
-
-def get_user_details(str_input):
-    cursor = connect()
-
-    #Query which searches the username exactly
-    cursor.execute("SELECT * FROM user WHERE screen_name = '%s' \
-    ORDER BY verified DESC, followers_count DESC"%str_input)
-
-    res = cursor.fetchall()
-    disconnect(cursor)
-    return res
+        res = cursor.fetchall()
+        self.disconnect(cursor)
+        return list(res)
